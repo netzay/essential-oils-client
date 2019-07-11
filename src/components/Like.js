be0d0ce66449641fd47a8ba8c05ee633636b5e28
netzay <netzelally@gmail.com>
2019-07-11 12:55:48 -0600

    add like

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import "../css/Like.css";
-import { connect } from 'react-redux';
-import { addFavorite } from '../actions/oils'
-
 
 class Like extends React.Component {
 
-  handleLike = event => {
-    event.preventDefault()
-    this.props.addFavorite(this.props.oil.id)
-    console.log("like")
+  state = {
+    isLiked: 'white'
+  }
+
+  handleLike = () => {
+    this.setState({
+      isLiked: 'blue'
+    });
   }
 
   render() {
     return (
       <span className="like-button">
-        <i className="fa fa-heart" onClick={this.handleLike} />
-
+        <i className="fa fa-heart" onClick={this.handleLike}
+          style={{ color: this.state.isLiked }} >
+        </i>
       </span>
     )
   }
 }
-export default connect(null, {
-  addFavorite
-})(Like);
+export default Like;
