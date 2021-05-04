7b9f558bad63666237fbb0e3482438ce9f3db01b
netzay <netzelally@gmail.com>
2021-05-04 18:31:48 -0400

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,17 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
 
   return (
-    <Navbar bg="danger" variant="light" sticky="top">
+    
+    <Navbar bg="danger" variant="light" navbar fixed-top>
       <Nav className="mr-auto">
         <Link to="/oils">Oils</Link>||
         <Link to="/about">About</Link>||
         <Link to="/addnew">New</Link>
       </Nav>
     </Navbar>
+
+
+
   )
 }
 export default NavBar;
